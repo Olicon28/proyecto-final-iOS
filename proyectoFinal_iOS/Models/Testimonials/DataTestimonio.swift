af7d98e0a3f3d3ab628fefa9c219add090b24c8c
Teresa <t.varas.cifuentes@gmail.com>
2022-06-23 14:45:21 -0400

    datatestimonioFix y vista

--- proyectoFinal_iOS/Models/Testimonials/DataTestimonio.swift
+++ proyectoFinal_iOS/Models/Testimonials/DataTestimonio.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct DataTestimonio: Decodable {
+
+    
+
+    var id: Int
+
+    var name: String
+
+    var image: String
+
+    var description: String
+
+    var created_at: String
+
+    var updated_at: String
+
+    var delated_at: String?
+
+    var group_id: Int?
+
+}
