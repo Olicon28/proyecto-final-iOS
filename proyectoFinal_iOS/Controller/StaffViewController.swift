32a120c9034d62f4dc37ab7e0b6fa1eb09063b3c
Alvaro Concha <alvaro.concha.c89@gmail.com>
2022-06-24 00:53:21 -0400

    se sube staff con carrusel

--- proyectoFinal_iOS/Controller/StaffViewController.swift
+++ proyectoFinal_iOS/Controller/StaffViewController.swift
@@ -10,6 +10,7 @@ import Alamofire
 
 
 extension UIImageView {
+    
     func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
         contentMode = mode
         URLSession.shared.dataTask(with: url) { data, response, error in
@@ -31,34 +32,39 @@ extension UIImageView {
 }
 
 
-class StaffViewController: UIViewController {
+class StaffViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    @IBOutlet weak var memberCollectionView: UICollectionView!
+    
     
     
     @IBOutlet weak var memberImage: UIImageView!
     @IBOutlet weak var nameMemberLabel: UILabel!
     @IBOutlet weak var roleMemberLabel: UILabel!
     
+    var pictureMembersCollection : [String] = []
+    var memberCollection : [MemberResponse] = []
+    
   
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     
         
         setupView()
+        
 
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        createTimer()
+    }
 
-    
-
-
-    
-    
     
     func setupView(){
-        print("Hola Members")
-        
         AF.request(EndPonints.members).response { response in
         
             
@@ -80,7 +86,7 @@ class StaffViewController: UIViewController {
                 }
                 
                 if let urlImg : String = selfData[0].image{
-                    print(urlImg)
+                    
                     self.memberImage.downloaded(from: urlImg)
                     self.memberImage.layer.cornerRadius = 16.0
                     self.memberImage.contentMode = .scaleAspectFill
@@ -92,9 +98,14 @@ class StaffViewController: UIViewController {
                 self.roleMemberLabel.text = String(htmlEncodedString: selfData[0].description)
                 
                 
-//                for member in selfData {
-//                    print("nombre_: ", member.name)
-//                }
+                for member in selfData {
+                    
+                    let person = MemberResponse(id: member.id, name: member.name, image: member.image ?? "", description: member.description, facebookUrl: member.facebookUrl, linkedinUrl: member.linkedinUrl, created_at: member.created_at, updated_at: member.updated_at, deleted_at: member.deleted_at ?? nil, group_id: member.group_id ?? 0)
+                    
+                    self.memberCollection.append(person)
+                    
+                    self.pictureMembersCollection.append(person.image ?? "")
+                }
 
             } catch let error {
                 
@@ -121,11 +132,101 @@ class StaffViewController: UIViewController {
             }
         }
     }
+    
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return self.pictureMembersCollection.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CarruselCollectionViewCell
+        
+        
+        cell.pictureImageView.downloaded(from: self.memberCollection[indexPath.row].image ?? "")
+        
+        cell.layer.cornerRadius = 6.6
+        
+        cell.pictureImageView.contentMode = .scaleAspectFill
+        cell.nameLabel.text = memberCollection[indexPath.row].name
+        cell.rolLabel.text =  String(htmlEncodedString: memberCollection[indexPath.row].description)
+
+        return cell
+        
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let picture = pictureMembersCollection[indexPath.row]
+        
+        for member in memberCollection{
+            
+            if let selfMember = member.image{
+                if(selfMember == picture){
+                    
+                        print(member)
+                    self.memberImage.downloaded(from: member.image ?? "")
+                    
+                    self.memberImage.layer.cornerRadius = 16.0
+                    self.memberImage.contentMode = .scaleAspectFill
+
+                    self.nameMemberLabel.text = member.name
+                    self.roleMemberLabel.text = String(htmlEncodedString: member.description)
+                    
+                    
+                }
+                
+            }
+        }
+    }
+    
+    
+    func createTimer(){
+        
+        let timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: false)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+3) {
+            timer.fire()
+        }
+    }
+    
+    
+    @objc func fireTimer() {
+        print("ReloadData")
+        memberCollectionView.reloadData()
+
+    }
         
 
         
         
+    @IBAction func onTapButton(_ sender: Any) {
         
+        let alert = UIAlertController(title: "Staff", message: "Ya eres parte!", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok" , style: .default, handler: { action in
+            switch action.style{
+                case .default:
+                    print("")
+                
+                case .cancel:
+                    print("")
+                    
+                case .destructive:
+                    print("")
+                    
+                @unknown default:
+                    print("")
+                
+            }
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+        
+
+    
     
 
 
---
+++ proyectoFinal_iOS/UI/CarruselCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CarruselCollectionViewCell.swift
+//  proyectoFinal_iOS
+//
+//  Created by alvaro.concha on 23-06-22.
+//
+
+import UIKit
+
+class CarruselCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var pictureImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var rolLabel: UILabel!
+}
